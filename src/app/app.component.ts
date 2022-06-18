d62f9aa32bd90a3d4ce55ed0b4fd27055987ba95
Dorel Veliu <dorel.v@icloud.com>
2022-06-18 14:33:56 +0200

    xxx

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -26,17 +26,17 @@ export class AppComponent implements AfterViewInit,OnInit {
 	}
 
 	ngAfterViewInit() : void {
-		let loader = document.getElementById('loader')!!;
-		let splash = document.getElementById('splash')!!;
+		//let loader = document.getElementById('loader')!!;
+		//let splash = document.getElementById('splash')!!;
 		let rightSection = document.getElementById('section-right')!!;
 		let leftSection = document.getElementById('section-left')!!;
 		setTimeout(()=>{
-			splash.remove();
+			//splash.remove();
 			rightSection.style.transform = "translateX(100%)";
 			leftSection.style.transform = "translateX(-100%)";
-			setTimeout(()=>{
-				loader.remove();
-			},800);
+			// setTimeout(()=>{
+			// 	loader.remove();
+			// },800);
 		},1000);
 	}
 }
